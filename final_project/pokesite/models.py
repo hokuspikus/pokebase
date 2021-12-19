c00e1efa690a030ae5ac0c8b16817b45af0831c9
Iwo Pikus <hokuspikus90@gmail.com>
2021-12-19 16:16:09 +0100

    semitested models, minor changes - null in evolve, add related names, add User import

--- final_project/pokesite/models.py
+++ final_project/pokesite/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 TYPES = (
     ("nor", "normal"),
@@ -31,32 +32,32 @@ TEAMS = (
 class Pokemon(models.Model):
     name = models.CharField(max_length=32, unique=True)
     pokedex_number = models.IntegerField(unique=True)
-    type = models.CharField(max_length=4, choices="TYPES")
-    second_type = models.CharField(max_length=4, choices="TYPES", null=True, blank=True)
-    evolves_from = models.ForeignKey("Pokemon", on_delete=models.CASCADE)
+    type = models.CharField(max_length=4, choices=TYPES)
+    second_type = models.CharField(max_length=4, choices=TYPES, null=True, blank=True)
+    evolves_from = models.ForeignKey("Pokemon", on_delete=models.CASCADE, null=True)
     image = models.ImageField(null=True)
     image_shiny = models.ImageField(null=True)
-    fast_attack = models.ForeignKey("FastAttack", on_delete=models.DO_NOTHING)
-    charged_attack = models.ForeignKey("ChargedAttack", on_delete=models.DO_NOTHING)
-    second_charged_attack = models.ForeignKey("ChargedAttack", on_delete=models.DO_NOTHING)
+    fast_attack = models.ForeignKey("FastAttack", on_delete=models.DO_NOTHING, null=True)
+    charged_attack = models.ForeignKey("ChargedAttack", on_delete=models.DO_NOTHING, null=True, related_name="primary_attack")
+    second_charged_attack = models.ForeignKey("ChargedAttack", on_delete=models.DO_NOTHING, null=True, related_name="secondary_attack")
 
 
 class FastAttack(models.Model):
     name = models.CharField(max_length=32, unique=True)
-    type = models.CharField(max_length=4, choices="TYPES")
+    type = models.CharField(max_length=4, choices=TYPES)
     power = models.IntegerField()
 
 
 class ChargedAttack(models.Model):
     name = models.CharField(max_length=32, unique=True)
-    type = models.CharField(max_length=4, choices="TYPES")
+    type = models.CharField(max_length=4, choices=TYPES)
     power = models.IntegerField()
 
 
 class Trainer(models.Model):
-    user = models.OneToOneField("User", on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=32, unique=True)
-    team = models.CharField(max_length=4, choices="TEAMS")
+    team = models.CharField(max_length=4, choices=TEAMS)
     pokemon = models.ManyToManyField("Pokemon", through="TrainerPokemon")
 
 
