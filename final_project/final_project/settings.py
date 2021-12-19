f683c8e1035f2231053a6bbee08fbba80fd0897a
Iwo Pikus <hokuspikus90@gmail.com>
2021-12-19 16:02:50 +0100

    add models (not tested), add Pillow to requirements.txt

--- final_project/final_project/settings.py
+++ final_project/final_project/settings.py
@@ -74,16 +74,6 @@ WSGI_APPLICATION = 'final_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'HOST': '127.0.0.1',
-#         'NAME': 'pokebase',
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'USER': 'postgres',
-#         'PASSWORD': 'postgres',
-#     }
-# }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
--- final_project/pokesite/models.py
+++ final_project/pokesite/models.py
@@ -1,3 +1,71 @@
 from django.db import models
 
+TYPES = (
+    ("nor", "normal"),
+    ("fig", "fighting"),
+    ("fly", "flying"),
+    ("gro", "ground"),
+    ("roc", "rock"),
+    ("bug", "bug"),
+    ("gho", "ghost"),
+    ("ste", "steel"),
+    ("fir", "fire"),
+    ("wat", "water"),
+    ("gra", "grass"),
+    ("ele", "electric"),
+    ("psy", "psychic"),
+    ("ice", "ice"),
+    ("dra", "dragon"),
+    ("fai", "fairy"),
+    ("dar", "dark"),
+)
+
+TEAMS = (
+    ("yel", "Instinct"),
+    ("blu", "Mystic"),
+    ("red", "Valor"),
+)
 # Create your models here.
+
+
+class Pokemon(models.Model):
+    name = models.CharField(max_length=32, unique=True)
+    pokedex_number = models.IntegerField(unique=True)
+    type = models.CharField(max_length=4, choices="TYPES")
+    second_type = models.CharField(max_length=4, choices="TYPES", null=True, blank=True)
+    evolves_from = models.ForeignKey("Pokemon", on_delete=models.CASCADE)
+    image = models.ImageField(null=True)
+    image_shiny = models.ImageField(null=True)
+    fast_attack = models.ForeignKey("FastAttack", on_delete=models.DO_NOTHING)
+    charged_attack = models.ForeignKey("ChargedAttack", on_delete=models.DO_NOTHING)
+    second_charged_attack = models.ForeignKey("ChargedAttack", on_delete=models.DO_NOTHING)
+
+
+class FastAttack(models.Model):
+    name = models.CharField(max_length=32, unique=True)
+    type = models.CharField(max_length=4, choices="TYPES")
+    power = models.IntegerField()
+
+
+class ChargedAttack(models.Model):
+    name = models.CharField(max_length=32, unique=True)
+    type = models.CharField(max_length=4, choices="TYPES")
+    power = models.IntegerField()
+
+
+class Trainer(models.Model):
+    user = models.OneToOneField("User", on_delete=models.CASCADE)
+    name = models.CharField(max_length=32, unique=True)
+    team = models.CharField(max_length=4, choices="TEAMS")
+    pokemon = models.ManyToManyField("Pokemon", through="TrainerPokemon")
+
+
+class TrainerPokemon(models.Model):
+    trainer = models.ForeignKey("Trainer", on_delete=models.CASCADE)
+    pokemon = models.ForeignKey("Pokemon", on_delete=models.CASCADE)
+    nickname = models.CharField(max_length=32, blank=True, null=True)
+    is_shiny = models.BooleanField()
+
+
+
+
