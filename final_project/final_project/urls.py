3608002a6fea14f55e66f112a5c1fc108460d3dd
Iwo Pikus <hokuspikus90@gmail.com>
2022-01-04 20:31:15 +0100

    add media and static settings, add media to urlpatterns, add blank=True in models, change in trainer_add.html to cards view with images

--- final_project/final_project/urls.py
+++ final_project/final_project/urls.py
@@ -16,9 +16,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from pokesite.views import BaseShowcase, TrainerAdd
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', BaseShowcase.as_view(), name="main_page"),
     path('trainer/<int:trainer_id>/', TrainerAdd.as_view(), name="main_page"),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- final_project/pokesite/models.py
+++ final_project/pokesite/models.py
@@ -35,12 +35,12 @@ class Pokemon(models.Model):
     pokedex_number = models.IntegerField(unique=True)
     type = models.CharField(max_length=4, choices=TYPES)
     second_type = models.CharField(max_length=4, choices=TYPES, null=True, blank=True)
-    evolves_from = models.ForeignKey("Pokemon", on_delete=models.CASCADE, null=True)
-    image = models.ImageField(null=True)
-    image_shiny = models.ImageField(null=True)
-    fast_attack = models.ForeignKey("FastAttack", on_delete=models.DO_NOTHING, null=True,
+    evolves_from = models.ForeignKey("Pokemon", on_delete=models.CASCADE, blank=True, null=True)
+    image = models.ImageField(upload_to="images", blank=True, null=True)
+    image_shiny = models.ImageField(blank=True, null=True)
+    fast_attack = models.ForeignKey("FastAttack", on_delete=models.DO_NOTHING, blank=True, null=True,
                                     related_name="default_primary")
-    charged_attack = models.ForeignKey("ChargedAttack", on_delete=models.DO_NOTHING, null=True,
+    charged_attack = models.ForeignKey("ChargedAttack", on_delete=models.DO_NOTHING, blank=True, null=True,
                                        related_name="default_secondary")
 
     def __str__(self):
@@ -71,7 +71,8 @@ class TrainerPokemon(models.Model):
     pokemon = models.ForeignKey("Pokemon", on_delete=models.CASCADE)
     nickname = models.CharField(max_length=32, blank=True, null=True)
     is_shiny = models.BooleanField(default=False)
-    fast_attack = models.ForeignKey("FastAttack", on_delete=models.DO_NOTHING, null=True)
-    charged_attack = models.ForeignKey("ChargedAttack", on_delete=models.DO_NOTHING, null=True, related_name="default")
-    second_charged_attack = models.ForeignKey("ChargedAttack", on_delete=models.DO_NOTHING, null=True,
+    fast_attack = models.ForeignKey("FastAttack", on_delete=models.DO_NOTHING, blank=True, null=True)
+    charged_attack = models.ForeignKey("ChargedAttack", on_delete=models.DO_NOTHING, blank=True, null=True,
+                                       related_name="default")
+    second_charged_attack = models.ForeignKey("ChargedAttack", on_delete=models.DO_NOTHING, blank=True, null=True,
                                               related_name="optional")
