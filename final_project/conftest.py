1228c29c38f16e7be54a6763b2a406eb1d0396a1
Iwo Pikus <hokuspikus90@gmail.com>
2022-01-21 23:47:48 +0100

    deleted unused fixtures

--- final_project/conftest.py
+++ final_project/conftest.py
@@ -5,5 +5,3 @@ from django.contrib.auth.models import User
 @pytest.fixture(scope='function')
 def user(db):
     return User.objects.create_user('TestUser', 'user@test.com', 'ThePassword1234')
-
-# @pytest.fixture(scope='function')
